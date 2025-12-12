1fae69c40ee207ba0a1430ab6a2089541f17796d
xishanht108 <lathanhsi100804@gmail.com>
2025-12-13 06:16:37 +0700

    Fix dotenv path for production

--- GameServer/server.js
+++ GameServer/server.js
@@ -1,5 +1,4 @@
-require('dotenv').config({ path: '/Users/xis108/Desktop/ntap/GameServer/.env' });
-const app = require('./src/app');
+require('dotenv').config();const app = require('./src/app');
 
 const PORT = process.env.PORT || 3000;
 
